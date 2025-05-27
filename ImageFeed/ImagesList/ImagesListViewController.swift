c8d31cdbdbdb4f27b69aa2fe7a75fa723a2097c6
AlexOldri <chudakov.aleksey96@yandex.ru>
2025-05-27 22:14:01 +0500

    Реализована лента изображений

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,70 @@
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    @IBOutlet private var tableView: UITableView!
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        let imageName = photosName[indexPath.row]
+        guard let image = UIImage(named: imageName) else {
+            return
+        }
+        cell.cellImageView.image = image
+        
+        let date = Date()
+        let formattedDate = dateFormatter.string(from: date)
+        cell.dateLabel.text = formattedDate
+        
+        let isEven = indexPath.row % 2 == 0
+        let likeButton = isEven ? "No Active" : "Active"
+        cell.likeButton.setImage(UIImage(named: likeButton), for: .normal)
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ImagesListCell", for: indexPath) as? ImagesListCell else {
+            return UITableViewCell()
+        }
+
+        configCell(for: cell, with: indexPath)
+        return cell
+    }
+}
